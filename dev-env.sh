cee64c47b2b7c37c05d0cb3db3bcaca9e10191ee
Pranav Cherukupalli <pcherukupall@juniper.net>
2019-08-13 14:14:46 -0700

    autoformat

--- dev-env.sh
+++ dev-env.sh
@@ -6,28 +6,28 @@ USER=
 DIR=
 
 docker run --privileged --name $DOCKER --restart "always" \
-      -w /root/contrail -itd \
-      -v /var/run/docker.sock:/var/run/docker.sock \
-      -v /Users/$USER$DIR/contrail:/root/contrail \
-      -v /Users/$USER$DIR/contrail-dev-env:/root/contrail-dev-env \
-      -v /Users/$USER/.gitconfig:/root/.gitconfig \
-      opencontrailnightly/developer-sandbox:master
-
+  -w /root/contrail -itd \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /Users/$USER$DIR/contrail:/root/contrail \
+  -v /Users/$USER$DIR/contrail-dev-env:/root/contrail-dev-env \
+  -v /Users/$USER/.gitconfig:/root/.gitconfig \
+  opencontrailnightly/developer-sandbox:master
 
 FILE=~/.bash_profile
 
 LINE="alias repo='docker exec $DOCKER repo'"
-grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >>"$FILE"
 
 LINE="alias scons='docker exec $DOCKER scons'"
-grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >>"$FILE"
 
 LINE="alias dev-sb='docker exec $DOCKER '"
-grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >>"$FILE"
 
 function dev_sb() {
   docker exec $DOCKER $@
 }
+
 dev_sb repo init --no-clone-bundle -u https://github.com/Juniper/contrail-vnc -b $BRANCH
 dev_sb repo sync --no-clone-bundle contrail-packages contrail-third-party
 dev_sb make -f tools/packages/Makefile dep
